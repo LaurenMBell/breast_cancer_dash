457844aa6638c57aee4869d39d894e94ccb91334
LaurenMBell <bell.laurenmar@gmail.com>
2025-10-08 13:23:25 -0700

    integrated data and built protoype without ML calculator

--- App.R
+++ App.R
@@ -11,100 +11,99 @@ library(ggplot2)
 library(dplyr)
 library(mapDK)
 
-source("bar_treatment.R")
+#source("bar_treatment.R")
+
+stage_theme <- c("1" = "yellow", 
+                 "2" = "orange", 
+                 "3" = "red", 
+                 "4" = "darkred")
+
+treatment_theme <- c("Chemotherapy" = "#E69F00",
+                     "Hormone Therapy" = "#56B4E9",
+                     "Radio Therapy" = "#009E73")
+
 
 cancer_data <- read_csv("data/METABRIC_RNA_Mutation.csv")
 
 ######################### UI #######################################
 
 ui <- page_navbar(
-  title = "Breast Cancer Data Dashboard",
-  
+  title = "Breast Cancer Dashboard",
   
   nav_panel(
-    title = "Breast Cancer Data",
+    title = "Clinical Data Exploration",
     layout_sidebar(
       sidebar = sidebar(
         title = "Selection Menu",
         
-        selectInput("type", 
+        selectInput("cancer_subtype", 
                     label = "Select breast cancer subtype:",
-                    choices = c("Invasive Ductal Carcinoma", 
-                                "Mixed Ductal and Lobular Carcinoma", 
-                                "Invasive Lobular Carcinoma", 
-                                "Invasive Mixed Mucinous Carcinoma", 
+                    choices = c("All Cancer Subtypes",
+                                "All Carcinoma Subtypes",
+                                "Breast Invasive Ductal Carcinoma", 
+                                "Breast Mixed Ductal and Lobular Carcinoma", 
+                                "Breast Invasive Lobular Carcinoma", 
+                                "Breast Invasive Mixed Mucinous Carcinoma", 
                                 "Metaplastic Breast Cancer"),
-                    selected = "Invasive Ductal Carcinoma"
-        ),
+                    selected = "All Cancer Subtypes"),
         
         sliderInput("age",
                     label = "Select age at diagnosis range:",
                     min = min(cancer_data$age_at_diagnosis), 
                     max = max(cancer_data$age_at_diagnosis),
-                    value = c(min, max)
-        ),
+                    value = c(min(cancer_data$age_at_diagnosis), 
+                              max(cancer_data$age_at_diagnosis))),
         
         checkboxGroupInput("surgery_type",
                            label = "Select surgery type:",
                            choices = c("Masectomy", "Breast Conserving"),
-                           selected = choices
-        ),
-        
-        
-      ),
-      
-      
-      layout_columns(
-        value_box(
-          title = "Patients (n)",
-          value = nrow(cancer_data_denmark) - 1, 
-          showcase = bsicons::bs_icon("capsule-pill"),
-          theme = "primary"
-        ), 
-        value_box(
-          title = "Average Age at Diagnosis in Years",
-          value = round(mean(cancer_data_denmark$Age, na.rm = TRUE), 1),
-          showcase = bsicons::bs_icon("clock"),
-          theme = "info"
-        ), 
-        value_box(
-          title = "Average Tumor Size in cm", 
-          value = round(mean(cancer_data_denmark$Tumor_Size_cm, na.rm = TRUE), 1), 
-          showcase = bsicons::bs_icon("arrows"), 
-          theme = "success"
-        ),
-        value_box(
-          title = "Average Survival in Months", 
-          value = round(mean(cancer_data_denmark$Survival_Months, na.rm = TRUE), 1), 
-          showcase = bsicons::bs_icon("calendar-event"), 
-          theme = "warning"
-        ),
-        value_box(
-          title = "Average BMI", 
-          value = round(mean(cancer_data_denmark$BMI, na.rm = TRUE), 1), 
-          showcase = bsicons::bs_icon("person"), 
-          theme = "secondary"
-        ),
-        card(
-          card_header("Treatments Given"),
-          plotOutput("bar_treatment")
-        ),
-        card(
-          card_header("Cancer Stage of Selected Cancer Type"),
-          plotOutput("bar_stage")
-        ),
-        card(
-          card_header("Number of Diagnoses Across Denmark"),
-          p("Number of Selected Cancer Diagnoses across Denmark"),
-          plotOutput("diagnosis_map")
-        ),
-        card(
-          p("put another graph here! something with dates")
+                           selected = c("Masectomy", "Breast Conserving")
         )
-      )
+      ), #closes data exploration selection menu
+      
+      fluidRow(
+        card(
+          card_header("Tumor Size for Selected Cancer Type"), 
+          plotOutput("tumor_size_hist")
+        )
+      ) # closes main page for data exploration
     )
-  ),
+  ),#closes data exploration page
+    
   
+  
+  nav_panel(
+    title = "Cohort Statistics", 
+    layout_columns(
+      value_box(
+        title = "Patients (n)",
+        value = nrow(cancer_data) - 1, 
+        showcase = bsicons::bs_icon("capsule-pill"),
+        theme = "primary"
+      ), 
+      value_box(
+        title = "Average Age at Diagnosis in Years",
+        value = round(mean(cancer_data$age_at_diagnosis, na.rm = TRUE), 1),
+        showcase = bsicons::bs_icon("clock"),
+        theme = "info"
+      ), 
+      value_box(
+        title = "Average Tumor Size in cm", 
+        value = round(mean(cancer_data$tumor_size, na.rm = TRUE), 1), 
+        showcase = bsicons::bs_icon("arrows"), 
+        theme = "success"
+      ),
+      value_box(
+        title = "Average Survival in Months", 
+        value = round(mean(cancer_data$overall_survival_months, na.rm = TRUE), 1), 
+        showcase = bsicons::bs_icon("calendar-event"), 
+        theme = "warning"
+      ),
+    ), #closes column layout
+    card(
+      plotOutput("cancer_subtype_bar")
+    )
+  ), #closes quick facts page
   
   nav_panel(
     title = "Survival Calculator",
@@ -112,7 +111,7 @@ ui <- page_navbar(
       card_header("Survival Time Prediction"),
       p("Survival calculator goes here...")
     )
-  )
+  ) #closes survival calculator page
 )
 
 
@@ -120,52 +119,54 @@ ui <- page_navbar(
 
 server <- function(input, output) {
   
-  output$bar_stage <- renderPlot({
-    summary <- cancer_data_denmark %>% 
-      filter(Cancer_Type == input$type) %>% 
-      count(Stage) 
+  filtered_data <- reactive({
+    data <- cancer_data
+    if (input$cancer_subtype == "All Cancer Subtypes") {
+      data <- data
+    } else if (input$cancer_subtype == "All Carcinoma Subtypes") {
+      data <- data %>%
+        filter(grepl("Carcinoma", cancer_type_detailed, ignore.case = TRUE))
+    } else {
+      data <- data %>%
+        filter(cancer_type_detailed == input$cancer_subtype)
+    }
+      data <- data %>%
+      filter(age_at_diagnosis >= input$age[1] & age_at_diagnosis <= input$age[2])
     
-    ggplot(summary, aes(x = Stage, y = n, fill = Stage)) + 
-      geom_col() +
-      scale_fill_manual(values = stage_theme) +
+    if (length(input$surgery_type) > 0) {
+      data <- data %>%
+        filter(toupper(type_of_breast_surgery) %in% toupper(input$surgery_type))
+    }
+    return(data)
+  })
+  
+  
+  #tumor size histogram: filter to selected type, age, and surgery type
+  output$tumor_size_hist <- renderPlot({
+    summary <- filtered_data()
+    
+    ggplot(summary, aes(x = tumor_size)) + 
+      geom_histogram(bins=15, fill="steelblue", color="white") +
       labs(
-        title = "# of Cancer Stage Diagnoses for Selected Cancer Type", 
-        x = "Cancer Stage", 
+        #title = "Distribution of Tumor Size\nfor Selected Cancer Type", 
+        x = "Tumor Size (cm)", 
         y = "Count") +
       theme_minimal()
   })
   
-  output$bar_treatment <- renderPlot({
-    summary <- cancer_data_denmark %>% 
-      filter(Cancer_Type == input$type) %>% 
-      count(Treatment_Type) 
-    
-    ggplot(summary, aes(x = reorder(Treatment_Type, n), y = n, fill = Treatment_Type)) + 
-      geom_col() +
-      coord_flip() + 
-      scale_fill_manual(values = treatment_theme) +
+  #cohort quick facts bar chart of cancer subtypes
+  output$cancer_subtype_bar <- renderPlot({
+    title = "Count of Cancer Subtypes"
+    cancer_data %>%
+      count(cancer_type_detailed) %>%
+      ggplot(aes(x = reorder(cancer_type_detailed, n), y = n)) + 
+      geom_col(fill = "steelblue") +
+      coord_flip() +
       labs(
-        title = "Treatments for Selected Cancer Type", 
-        x = "Treatment", 
-        y = "Count") +
+        x = "Cancer Subtype", 
+        y = "Count"
+      ) +
       theme_minimal()
-  })
-  
-  output$diagnosis_map <- renderPlot({
-    selected_type <- cancer_data_denmark %>% filter(Cancer_Type == input$type)
-    region <- c("region hovedstaden", "region sjælland","region syddanmark", "region midtjylland", "region nordjylland")
-    count <- c(sum(selected_type$Region == "Hovedstaden"),
-               sum(selected_type$Region == "Sjælland"), 
-               sum(selected_type$Region == "Syddanmark"), 
-               sum(selected_type$Region == "Midtjylland"),
-               sum(selected_type$Region == "Nordjylland"))
-    
-    city_data <- data.frame(region, count)
-    
-    mapDK(
-      detail = "region", 
-      data = city_data, id = "region", values = "count", 
-      guide.label = "Number of Diagnoses")  
   })
   
 }
