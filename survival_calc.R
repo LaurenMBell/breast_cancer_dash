0fa1cd1082c9c493e6f105f51de6fb0376ad47c7
LaurenMBell <bell.laurenmar@gmail.com>
2026-04-22 16:32:21 -0700

    finished classifier and realized I have way too many features

--- survival_calc.R
+++ survival_calc.R
@@ -10,11 +10,22 @@ library(dplyr)
 
 data <- read.csv('data/METABRIC_RNA_Mutation.csv')
 data <- data[,-1] #drop patient ID
-data %>% relocate(overall_survival)
+data$oncotree_code <- NULL
+data$primary_tumor_laterality <- NULL
+data$integrative_cluster <- NULL
+
+data <- data %>% relocate(overall_survival)
+data <- na.omit(data)
 
 ### map all text features to discrete 
-
-as.factor(data$type_of_breast_surgery)
+data <- data %>%
+  mutate(across(c(cancer_type, cancer_type_detailed, cellularity, 
+                  type_of_breast_surgery, pam50_._claudin.low_subtype, 
+                  er_status_measured_by_ihc, er_status, 
+                  her2_status_measured_by_snp6, her2_status,
+                  tumor_other_histologic_subtype, inferred_menopausal_state, 
+                  pr_status, X3.gene_classifier_subtype, 
+                  death_from_cancer), as.factor))
 
 
 set.seed(123)
@@ -22,11 +33,7 @@ split <- sample.split(data, SplitRatio=0.7)
 train <- subset(data, split == TRUE)
 test <- subset(data, split == FALSE)
 
-train_scale <- scale(train[,-1])
-test_scale <- scale(test[,-1])
-
-s_classifier <- naiveBayes(overall_survival ~ ., data = data)
-classifier
+s_classifier <- naiveBayes(overall_survival ~ ., data = train)
 
 y_pred <- predict(s_classifier, newdata=test)
 
