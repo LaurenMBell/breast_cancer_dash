0bec2f4e5173c3e7396844832a74cbf0ca9ba228
LaurenMBell <bell.laurenmar@gmail.com>
2026-04-22 16:19:05 -0700

    added classifier skeleton code and discretizing features

--- survival_calc.R
+++ survival_calc.R
@@ -1,6 +1,38 @@
 # survival calculator for the breast cancer transcriptomics dashboard! 
 #naive bayes model, 1=survived, 0=died
 
-METABRIC <- read.csv('data/METABRIC_RNA_Mutation.csv')
+#next goal: implement naive bayes without a library import
 
-# check current distributions for transcriptomics features
+library(e1071)
+library(caTools)
+library(caret)
+library(dplyr)
+
+data <- read.csv('data/METABRIC_RNA_Mutation.csv')
+data <- data[,-1] #drop patient ID
+data %>% relocate(overall_survival)
+
+### map all text features to discrete 
+
+as.factor(data$type_of_breast_surgery)
+
+
+set.seed(123)
+split <- sample.split(data, SplitRatio=0.7)
+train <- subset(data, split == TRUE)
+test <- subset(data, split == FALSE)
+
+train_scale <- scale(train[,-1])
+test_scale <- scale(test[,-1])
+
+s_classifier <- naiveBayes(overall_survival ~ ., data = data)
+classifier
+
+y_pred <- predict(s_classifier, newdata=test)
+
+#confusion matrix
+cm <- table(test$overall_survival, y_pred)
+confusionMatrix(cm)
+
+
+
